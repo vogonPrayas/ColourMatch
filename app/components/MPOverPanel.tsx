c477385f238f6d901e339c44fef58c798b01987b
vogonPrayas <prayasgautam996@gmail.com>
2024-05-11 00:52:37 +0100

    bug fix

--- app/components/MPOverPanel.tsx
+++ app/components/MPOverPanel.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import useStore from '../store'
 import { State } from '../store'
--- app/components/MultiplayerBox.tsx
+++ app/components/MultiplayerBox.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react'
 import OtherBox from './otherBox'
 import "../css/randomBox.css"
